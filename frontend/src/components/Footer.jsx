96c899eed3ccd44ebfd207c574356b40ce788a49
Flebory <123499686+Flebory@users.noreply.github.com>
2025-03-02 20:09:27 -0500

    Updated Footer Text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <div className = "footer p-6 bg-gray-100 text-gray-800 shadow-md flex flex-col md:flex-row items-center justify-center">
             {/* Copyright */}
             <div className = "flex items-center space-x-2">
-                    <span className = "text-lg font-bold">© 2025 MedChatbot. All rights reserved.</span>
+                    <span className = "text-lg font-bold">© 2025 PraxAssist. All rights reserved.</span>
             </div>
         </div>
     );
