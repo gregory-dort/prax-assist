3d700b7dcc5d3d0e885ad794cfd17307490ac6ec
Flebory <123499686+Flebory@users.noreply.github.com>
2025-03-23 20:45:22 -0400

    Commented out aiRoutes in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 // Importing DB Elements & Routes
 const authRoutes = require("./routes/authRoutes");
-const aiRoutes = require("./routes/aiRoutes");
+//const aiRoutes = require("./routes/aiRoutes");
 const connectDB = require("./config/db");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
